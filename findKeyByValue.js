58dfd39902b9c54f2f30c6c41cdc611d502bf151
triple-eh <aleckaleksandrov@gmail.com>
2020-03-24 10:40:09 -0700

    created function to find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙌🙌🙌Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤔🤔🤔Assertion failed: ${actual} <> ${expected}`);
+  }
+};
+
+const findKeyByValue = function(searchObject, value) {
+  let result = undefined;
+  for (const key in searchObject) {
+    if (searchObject[key] === value) {result = key};
+  }
+  return result;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
