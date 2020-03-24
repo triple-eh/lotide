22178f3c78d6447dab3ba83b8041b857fd920e6b
triple-eh <aleckaleksandrov@gmail.com>
2020-03-24 10:50:46 -0700

    added eqObjects function

---
+++ eqObjects.js
@@ -0,0 +1,30 @@
+const eqObjects = function(object1, object2) {
+
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let k in object1) {
+      if (object1[k] instanceof Object) {
+        if (!eqObjects(object1[k], object2[k])) {
+          return false;
+        }
+      } else {
+        if (object1[k] !== object2[k]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
+
+
+module.exports = eqObjects;
