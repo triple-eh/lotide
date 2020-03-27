c75fc67f7bd08ee7a57e57b1dbfd79ef23f2ff3d
triple-eh <aleckaleksandrov@gmail.com>
2020-03-27 10:27:26 -0700

    created findKey function

---
+++ findKey.js
@@ -0,0 +1,18 @@
+const findKey = (obj, cb) => {
+  for (const el in obj) {
+    if (cb(obj[el])) {
+      return el;
+    }
+  }
+};
+
+console.log(
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+);
