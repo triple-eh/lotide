e2c75af171998f28a0953718944449c29effb238
triple-eh <aleckaleksandrov@gmail.com>
2020-03-31 09:06:58 -0700

    refactored index.js using es6 object shorthand

--- index.js
+++ index.js
@@ -2,8 +2,4 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 
-module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
-};
+module.exports = {head, tail, middle};
